f7cc85c40e9e5ca11b2c6b4c23fb8b76f994e43a
krysiajanik <a.krystyna.janik@gmail.com>
2021-02-23 21:05:52 +0100

    dropdown menu rewritten with NavLink/BrowserRouter plus styling and animation

--- src/components/Header/MenuDropdown.js
+++ src/components/Header/MenuDropdown.js
@@ -1,19 +1,51 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./MenuDropdown.module.scss";
 
 const MenuDropdown = () => (
- <div className={styles.wrapper}>
-     <p className={styles.menuItem}>
-        Co jemy?
-     </p>
-     <p className={styles.menuItem}>
-        Co pijemy?
-     </p>
-     <p className={styles.menuItem}>
-        Gdzie siedzę?
-     </p>
+  <nav>
+    <ul className={styles.wrapper}>
+      <li className={styles.menuItem}>
+        <NavLink exact
+          activeClassName={styles.menuItemActive}
+          to="/"
+          className={styles.menuItemLink}
+        >
+            Strona główna
+        </NavLink>
+      </li>
+      <li className={styles.menuItem}>
+        <NavLink
+          activeClassName={styles.menuItemActive}
+          to="/menu"
+          className={styles.menuItemLink}
+        >
+            Co jemy?
+        </NavLink>
+      </li>
+      
+      <li className={styles.menuItem}>
+        <NavLink
+          activeClassName={styles.menuItemActive}
+          to="/drinkmenu"
+          className={styles.menuItemLink}
+        >
+            Co pijemy?
+        </NavLink>
+      </li>
+     
+      <li className={styles.menuItem}>
+        <NavLink
+          activeClassName={styles.menuItemActive}
+          to="/sittingchart"
+          className={styles.menuItemLink}
+        >
+            Gdzie siedzę?
+        </NavLink>
+      </li>
+      
+    </ul>
+  </nav>
+);
 
- </div>
-)
-
-export default MenuDropdown
+export default MenuDropdown;
