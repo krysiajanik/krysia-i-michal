3d724acccecb91e5aad339449a6d60a3b12499e9
krysiajanik <a.krystyna.janik@gmail.com>
2021-03-07 19:54:50 +0100

    function for various classnames for chairs depending on placement

--- src/components/SittingPlan/Chair/Chair.js
+++ src/components/SittingPlan/Chair/Chair.js
@@ -11,11 +11,47 @@ function Chair(props) {
   const selectedPerson = useContext(AppContext);
   const tableNr = props.tableNr;
   const chairNr = props.chairNr;
+  const row = props.row;
+
+  function personClass() {
+    let newClass;
+    if (isShown === true) {
+      switch (row) {
+        case "up":
+          newClass = "styles.personShowUp";
+          console.log(newClass);
+          break;
+        case "down":
+          newClass = "styles.personShowDown";
+          console.log(newClass);
+          break;
+        case "left":
+          newClass = "styles.personShowLeft";
+          console.log(newClass);
+          break;
+        case "right":
+          newClass = "styles.personShowRight";
+          console.log(newClass);
+          break;
+        default:
+          newClass = "styles.personShow";
+      }
+      return newClass;
+    } else {
+      newClass = "styles.personHide";
+      return newClass;
+    }
+  }
+
+  // "up";
+  //   const rowDown = "down";
+  //   const rowLeft = "left";
+  //   const rowRight = "right";
 
   return (
     <div
       className={
-        (selectedPerson.tableNr === tableNr && selectedPerson.chairNr === chairNr)
+        selectedPerson.tableNr === tableNr && selectedPerson.chairNr === chairNr
           ? styles.chairHighlight
           : styles.chair
       }
@@ -25,7 +61,7 @@ function Chair(props) {
       {SittingArray.map((person) => (
         <p
           key={`${person.tableNr}_${person.chairNr}`}
-          className={isShown ? styles.personShow : styles.personHide}
+          className={personClass(row)}
         >
           {person.tableNr === tableNr && person.chairNr === chairNr
             ? `${person.firstName} ${person.lastName}`
@@ -35,6 +71,8 @@ function Chair(props) {
     </div>
   );
 }
+
+// isShown ? styles.personShow : styles.personHide
 
 Chair.propTypes = {
   chairNr: PropTypes.number.isRequired,
--- src/components/SittingPlan/SittingPlan.js
+++ src/components/SittingPlan/SittingPlan.js
@@ -1,38 +1,31 @@
+import { element } from "prop-types";
 import React from "react";
-import AppContext from "../../context";
 import Chair from "./Chair/Chair";
 import styles from "./SittingPlan.module.scss";
 import Table from "./Table/Table";
 
 class SittingPlan extends React.Component {
-  static contextType = AppContext;
-
-//   state = {
-//     selectedChair: this.context.chairNr,
-//     selectedTable: this.context.tableNr,
-//   };
-
-//   checkSelected = () => this.state.selectedTable === tableNr && (this.state.selectedChair = chairNr);
-
-  
   render() {
-      
+    const rowUp = "up";
+    const rowDown = "down";
+    const rowLeft = "left";
+    const rowRight = "right";
+
     return (
       <div className={styles.wrapper}>
-        <div className={styles.tableGrid} >
-            
+        <div className={styles.tableGrid}>
           <div className={styles.chairRow}>
-            <Chair chairNr={1} tableNr={2}></Chair>
-            <Chair chairNr={2} tableNr={2}></Chair>
-            <Chair chairNr={3} tableNr={2}></Chair>
-            <Chair chairNr={4} tableNr={2}></Chair>
+            <Chair chairNr={1} tableNr={2} row={rowUp}></Chair>
+            <Chair chairNr={2} tableNr={2} row={rowUp}></Chair>
+            <Chair chairNr={3} tableNr={2} row={rowUp}></Chair>
+            <Chair chairNr={4} tableNr={2} row={rowUp}></Chair>
           </div>
           <Table tableNr={2} tableCapacity={8}></Table>
           <div className={styles.chairRow}>
-            <Chair chairNr={5} tableNr={2}></Chair>
-            <Chair chairNr={6} tableNr={2}></Chair>
-            <Chair chairNr={7} tableNr={2}></Chair>
-            <Chair chairNr={8} tableNr={2}></Chair>
+            <Chair chairNr={5} tableNr={2} row={rowDown}></Chair>
+            <Chair chairNr={6} tableNr={2} row={rowDown}></Chair>
+            <Chair chairNr={7} tableNr={2} row={rowDown}></Chair>
+            <Chair chairNr={8} tableNr={2} row={rowDown}></Chair>
           </div>
         </div>
       </div>
