30595affc936c6eede220e21f6b2c57c7b32070f
krysiajanik <a.krystyna.janik@gmail.com>
2021-04-01 21:21:36 +0200

    Site responsiveness improved with @media added with breakpoint at 480px

--- src/components/SittingPlan/SittingPlan.js
+++ src/components/SittingPlan/SittingPlan.js
@@ -3,20 +3,30 @@ import styles from "./SittingPlan.module.scss";
 import Table from "./Table/Table";
 
 class SittingPlan extends React.Component {
+  // resizeView = () => {
+  //   const el = document.getElementById("wrapper");
+  //   const elWidth = el.getBoundingClientRect().width;
+  //   const winWidth = window.innerWidth;
+  //   const scale = elWidth / winWidth;
+  //   return {
+  //     transform: "scale(" + scale + ")",
+  //   };
+  // };
+
   render() {
     return (
-      <div className={styles.wrapper}>
-        <div className={styles.tableMain}>
-          <Table assignedTableNr={1}></Table>
+        <div className={styles.wrapper}>
+          <div className={styles.tableMain}>
+            <Table assignedTableNr={1}></Table>
+          </div>
+          <Table assignedTableNr={2}></Table>
+          <Table assignedTableNr={3}></Table>
+          <Table assignedTableNr={4}></Table>
+          <Table assignedTableNr={6}></Table>
+          <Table assignedTableNr={7}></Table>
+          <Table assignedTableNr={8}></Table>
+          <Table assignedTableNr={9}></Table>
         </div>
-        <Table assignedTableNr={2}></Table>
-        <Table assignedTableNr={3}></Table>
-        <Table assignedTableNr={4}></Table>
-        <Table assignedTableNr={6}></Table>
-        <Table assignedTableNr={7}></Table>
-        <Table assignedTableNr={8}></Table>
-        <Table assignedTableNr={9}></Table>
-      </div>
     );
   }
 }
