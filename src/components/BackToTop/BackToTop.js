f4e132b4709fd30f035b673c21c7f986f042390c
krysiajanik <a.krystyna.janik@gmail.com>
2021-04-06 13:08:57 +0200

    Back to Top button functionality and styling finished

--- src/components/BackToTop/BackToTop.js
+++ src/components/BackToTop/BackToTop.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 // import PropTypes from "prop-types";
 import styles from "./BackToTop.module.scss";
 
 function BackToTop() {
+  const [showButton, setShowButton] = useState(false);
+  const winHeight = window.innerHeight / 2;
+
+  const handleShowButton = () => {
+    if (!showButton && window.pageYOffset > winHeight) {
+      setShowButton(true);
+    } else if (showButton && window.pageYOffset <= winHeight) {
+      setShowButton(false);
+    }
+  };
+  window.addEventListener("scroll", handleShowButton);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behaviour: "smooth" });
+  };
+
   return (
-    <button className={styles.buttonUp}>
+    <button
+      onClick={scrollToTop}
+      className={showButton ? styles.buttonUp : styles.noButton}
+    >
       <svg height={24} width={24} fill="#013A1E">
         <path d="M0 0h24v24H0V0z" fill="none" />
         <path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z" />
