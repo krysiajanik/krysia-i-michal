e62a277132e31769270de123093f3eb7cf4723b9
krysiajanik <a.krystyna.janik@gmail.com>
2021-07-06 20:58:59 +0200

    Simple authentication added to author name to be minimum 3 characters

--- src/components/Gallery/UploadForm/UploadForm.js
+++ src/components/Gallery/UploadForm/UploadForm.js
@@ -11,48 +11,64 @@ const UploadForm = () => {
   const types = ["image/png", "image/jpeg", "image/jpg", "image/gif"];
 
 
- 
-  const handleChange = (e) => {
+
+  const handleImage = (e) => {
     let selected = e.target.files[0];
 
     if (selected && types.includes(selected.type)) {
       setTempFile(selected);
       setError('');
     } else {
-        setTempFile(null);
-        setError('Wybierz zdjęcie w jednym z dozwolonych formatów! (png, jpeg, jpg, gif)');
+      setTempFile(null);
+      setError('Wybierz zdjęcie w jednym z dozwolonych formatów! (png, jpeg, jpg, gif)');
     }
   };
+
+  const validateAuthor = () => {
+
+
+  }
 
   const addFile = (e) => {
     e.preventDefault()
 
     const newFile = tempFile
-    newFile.authorName = authName
-    setFile(newFile)
-    setTempFile(null)
+
+    if (!authName) {
+      window.alert("Podpisz swoje zdjęcie")
+    }
+
+    if (authName && authName.length < 3) {
+      window.alert("Imię musi mieć przynajmniej trzy znaki")
+    } else {
+      newFile.authorName = authName
+      setFile(newFile)
+      setTempFile(null)
+      setAuthName(null)
+    }
+
   }
 
   return (
     <form className={styles.form}>
-        <label className={styles.formLabel}>
-        <input type="file"  onChange={handleChange} />
+      <label className={styles.formLabel}>
+        <input type="file" onChange={handleImage} />
         <span className={styles.addButton}>+</span>
-        </label>
-     
+      </label>
+
       <div className={styles.output}>
-          {error && <p className={styles.error}>{error}</p>}
-          {tempFile && 
+        {error && <p className={styles.error}>{error}</p>}
+        {tempFile &&
           <>
-          <p className={styles.filename}>{tempFile.name}</p>
-          <input className={styles.authorName} required type="text" placeholder="Twoje imię" id="authorName" onChange={(e)=>setAuthName(e.target.value)}></input>
-          <button className={styles.formButton} onClick={addFile}>DODAJ</button>
+            <p className={styles.filename}>{tempFile.name}</p>
+            <input className={styles.authorName} required min="2" max="30" type="text" placeholder="Twoje imię" id="authorName" onChange={(e) => setAuthName(e.target.value)}></input>
+            <button className={styles.formButton} onClick={addFile}>DODAJ</button>
           </>
-      }
-          {file && <ProgressBar file={file} setFile={setFile} />}
+        }
+        {file && <ProgressBar file={file} setFile={setFile} />}
       </div>
 
-      
+
     </form>
   );
 };
